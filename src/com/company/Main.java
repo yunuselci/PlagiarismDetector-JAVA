697f6a2b22159d2d90c455e8719095842cc51675
unknown <mryunuselci@gmail.com>
2021-01-16 05:02:26 +0300

    WIP:Total score of documents
    +Sort fonksiyonu değişti
    +Print Fonksiyonu değişti
    +Hashmap key,value yapısı değişti.
    +Kod okunarlığı arttırıldı

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,21 @@ import java.util.*;
 public class Main {
     final static File databaseFolder = new File("C:\\Users\\YUNUS\\IdeaProjects\\Cmp3005\\database");//Give main_doc.txt path here
     final static File controlFolder = new File("C:\\Users\\YUNUS\\IdeaProjects\\Cmp3005\\control");//Give controlFolder path here
-    static ArrayList<String> databaseSentences = new ArrayList<>();
-    static ArrayList<String> controlSentences = new ArrayList<>();
-    static LinkedHashMap<Double, String> hash_map = new LinkedHashMap<>();
-    static ArrayList<String> controlFolderAbsolutePaths = new ArrayList<>();
+    static ArrayList<String> databaseSentences = new ArrayList<>(); //Hold the main file sentences
+    static ArrayList<String> controlSentences = new ArrayList<>();  //Hold the sentences to check
+    static HashMap<String, Double> hash_map = new HashMap<>(); //Similarity rate with sentences
+    static ArrayList<String> controlFolderAbsolutePaths = new ArrayList<>(); //Absolute path of control folders
 
 
     static void readTheDatabaseDirectory(final File folder) throws FileNotFoundException {
+        //This method assumes that there will be only one txt file in the database folder.
         for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
             if (fileEntry.isDirectory()) {
                 readTheDatabaseDirectory(fileEntry);
             } else {
-                Scanner sentence = new Scanner(new File(fileEntry.getAbsolutePath()));
-                sentence.useDelimiter("(?<=[.!?])\\s*");
-                while (sentence.hasNextLine())
-                {
+                Scanner sentence = new Scanner(new File(fileEntry.getAbsolutePath())); //standart scanner operation
+                sentence.useDelimiter("(?<=[.!?])\\s*"); //to read sentences
+                while (sentence.hasNextLine()) {
                     databaseSentences.add(sentence.next());
                 }
                 sentence.close();
@@ -29,6 +29,7 @@ public class Main {
     }
 
     static void readTheControlDirectory(final File folder) {
+        //This method does not read any sentences. It just puts the exact paths of the files to be compared in the arraylist
         for (final File fileEntry : Objects.requireNonNull(folder.listFiles())) {
             if (fileEntry.isDirectory()) {
                 readTheControlDirectory(fileEntry);
@@ -40,10 +41,9 @@ public class Main {
 
 
     static void readTheSentences(final String directory) throws FileNotFoundException {
-        Scanner sentence = new Scanner(new File(directory));
-        sentence.useDelimiter("(?<=[.!?])\\s*");
-        while (sentence.hasNextLine())
-        {
+        Scanner sentence = new Scanner(new File(directory)); //standart scanner operation
+        sentence.useDelimiter("(?<=[.!?])\\s*"); //to read sentences
+        while (sentence.hasNextLine()) {
             controlSentences.add(sentence.next());
         }
         sentence.close();
@@ -52,102 +52,119 @@ public class Main {
     public static double similarity(String s1, String s2) {
         String longer = s1, shorter = s2;
         if (s1.length() < s2.length()) { // longer should always have greater length
-            longer = s2; shorter = s1;
+            longer = s2;
+            shorter = s1;
         }
         int longerLength = longer.length();
-        if (longerLength == 0) { return 1.0; /* both strings are zero length */ }
-    /* // If you haves Apache Common Text, you can use it to calculate the edit distance:
-    LevenshteinDistance levenshteinDistance = new LevenshteinDistance();
-    return (longerLength - levenshteinDistance.apply(longer, shorter)) / (double) longerLength; */
-        return 100*(longerLength - editDistance(longer, shorter)) / (double) longerLength;
+        if (longerLength == 0) {
+            return 1.0;
+        }
+
+        return (100 * (longerLength - editDistance(longer, shorter)) / (double) longerLength);
 
     }
-    // Example implementation of the Levenshtein Edit Distance
-    // See http://rosettacode.org/wiki/Levenshtein_distance#Java
-    public static int editDistance(String s1, String s2) {
+
+    public static int editDistance(String s1, String s2) { //Levenshtein distance algorithm implementation
         s1 = s1.toLowerCase();
         s2 = s2.toLowerCase();
-
+        //mean of costs "cost to convert one string to another"
         int[] costs = new int[s2.length() + 1];
         for (int i = 0; i <= s1.length(); i++) {
             int lastValue = i;
             for (int j = 0; j <= s2.length(); j++) {
-                if (i == 0)
+                if (i == 0) {
                     costs[j] = j;
-                else {
+                } else {
                     if (j > 0) {
                         int newValue = costs[j - 1];
-                        if (s1.charAt(i - 1) != s2.charAt(j - 1))
-                            newValue = Math.min(Math.min(newValue, lastValue),
-                                    costs[j]) + 1;
+                        if (s1.charAt(i - 1) != s2.charAt(j - 1)) {
+                            newValue = Math.min(Math.min(newValue, lastValue), costs[j]) + 1;
+                        }
                         costs[j - 1] = lastValue;
                         lastValue = newValue;
                     }
                 }
             }
-            if (i > 0)
+            if (i > 0) {
                 costs[s2.length()] = lastValue;
+            }
         }
+        //We get costs from this implementation and use to calculate similarity between two string
         return costs[s2.length()];
     }
 
     public static void putIntoHashMap(String sentence1, String sentence2) {
-        hash_map.put(similarity(sentence1,sentence2),sentence2);
+        //Create <key,value> structure
+        //you can run this put method to see which sentences are compared
+        hash_map.put((sentence2 + "\tBETWEEN:" + sentence1), similarity(sentence1, sentence2));
+        //hash_map.put(similarity(sentence1,sentence2),sentence2);
+
 
     }
 
-    public static void sortTheHashMapByTheKeyAndPrintTheValue(int counter)
-    {
-        ArrayList<Double> sortedKeys =
-                new ArrayList<>(hash_map.keySet());
-        Collections.sort(sortedKeys);
-        Collections.reverse(sortedKeys);
-        double total=0.0;
-        double total2=0.0;
-        for (Double x : sortedKeys){
-            total += x;
+    public static void sortByValue() {
+    //convert HashMap into List
+        List<Map.Entry<String, Double>> list = new LinkedList<>(hash_map.entrySet());
+    //sorting the list elements
+        list.sort((o1, o2) -> o2.getValue().compareTo(o1.getValue()));
+    //prints the sorted HashMap
+        Map<String, Double> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Double> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
         }
-        total2 = total/ hash_map.size();
-        System.out.println("Similarity Rate For Document:"+counter+" %"+total2);
-        int i=0;
-        for (Double x : sortedKeys){
+        printMap(sortedMap);
+    }
+
+    //method for printing the elements
+    public static void printMap(Map<String, Double> map) {
+        double sumOfKeys=0.0, similarityRateOfWholeDocument = 0.0;
+        for (Map.Entry<String, Double> entry : map.entrySet()) {
+            sumOfKeys += entry.getValue();
+        }
+        similarityRateOfWholeDocument = sumOfKeys / hash_map.keySet().size(); //measure similarity rate of document
+        System.out.println("Similarity Rate For Document:"+" %"+similarityRateOfWholeDocument);
+        int i = 0;
+        for (Map.Entry<String, Double> entry : map.entrySet()) {
             i++;
             if(i<=5){
                 System.out.println(
-                        "Folder = " + counter +
-                        ", Similarity Score = %" + Math.round(x) +
-                        ", "+i+"." + "Sentence = "+ hash_map.get(x));
+                        "Similarity Score = %" + Math.round(entry.getValue()) +
+                                ", "+i+"." + "Sentence = "+ entry.getKey());;
             }
         }
-
-
-
     }
 
 
     public static void main(String[] args) throws IOException {
-        long startTime = System.currentTimeMillis();
-        int counter = 0; //Bu counter çıktı da düzgün bir görüntü sağlansın diye bulunuyor, olmasa da olur
+        long startTime = System.currentTimeMillis(); //to measure run time
         readTheDatabaseDirectory(databaseFolder);
         readTheControlDirectory(controlFolder);
-        for (int index = 0; index < controlFolderAbsolutePaths.size(); index++) {
+        System.out.println("If the similarity rate more than 60 percent we can be regarded as a plagiarism.");
+        for (int index = 0; index < controlFolderAbsolutePaths.size(); index++) { //loop1
+            //The 1st loop will iterate the number of files to be checked
             readTheSentences(controlFolderAbsolutePaths.get(index));
-            for (int j = 0; j < databaseSentences.size(); j++) {
-                for (int i = 0; i < controlSentences.size(); i++) {
+            for (int j = 0; j < databaseSentences.size(); j++) { //loop2
+                for (int i = 0; i < controlSentences.size(); i++) { //loop3
+                    /*
+                    Main algorithm work in here
+                    Through the 2nd loop, we can compare the first sentence of the database and all the sentences of the document to be controlled in the 3rd loop.
+                     */
                     putIntoHashMap(databaseSentences.get(j), controlSentences.get(i));
-
                 }
             }
             System.out.println(" ");
-            ++counter;
-            sortTheHashMapByTheKeyAndPrintTheValue(counter);
-            controlSentences.clear();
+            System.out.println("-------------------");
+            System.out.println(controlFolderAbsolutePaths.get(index));
+            System.out.println("-------------------");
+            sortByValue();
+            /*
+            after compare 2 files and print them we have to release the hash_map and arraylist to fill with new values
+             */
             hash_map.clear();
             controlSentences.clear();
         }
+        //to measure run time
         long endTime = System.currentTimeMillis();
         System.out.println("That took " + (endTime - startTime) + " milliseconds");
-
-
     }
 }
